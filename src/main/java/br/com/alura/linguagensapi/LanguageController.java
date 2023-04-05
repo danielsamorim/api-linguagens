5e1c6b725af9bab765b5bdbe9aeb03063dee202b
Daniel <danielamorim098@gmail.com>
2023-04-04 21:34:12 -0300

    api criada

---
+++ src/main/java/br/com/alura/linguagensapi/LanguageController.java
@@ -0,0 +1,60 @@
+package br.com.alura.linguagensapi;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class LanguageController {
+  
+  @Autowired
+  private LanguageRepository repository;
+
+
+  @GetMapping("/linguagens")
+  public List<Language> getAll(){
+    return repository.findByOrderByRanking();
+  }
+
+  @GetMapping("/linguagens/{id}")
+  public Language busca(@PathVariable String id){
+    
+    return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+  }
+
+  @PostMapping("/linguagens")
+  public HttpStatus  createLanguage(@RequestBody Language language){
+    
+    repository.save(language);
+    return HttpStatus.CREATED;
+    
+  }
+
+   @PutMapping("/linguagens/{id}")
+   public Language updateLanguage(@PathVariable String id, @RequestBody Language language){
+    if(!repository.existsById(id)){
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+    language.setId(id);
+     Language lang = repository.save(language);
+     return lang;
+  }
+
+  @DeleteMapping("/linguagens/{id}")
+  public void deleteLanguage(@PathVariable String id){
+    if(!repository.existsById(id)){
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+    repository.deleteById(id);
+  }
+
+
+}
